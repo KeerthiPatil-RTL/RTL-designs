2b0d1352743e11544d67e465a97ec0b5e276e307
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-09 18:02:31 -0400

    Create TB.v[

---
+++ HA/TB.v
@@ -0,0 +1,14 @@
+module HA_TB;
+reg a,b;
+wire sum,carry;
+HABEH uut(.a(a), .b(b), .sum(sum), .carry(carry));
+initial begin
+$monitor("time=%0t | a=%b b=%b | sum=%b carry=%b", $time, a, b, sum, carry);
+#5 a=0;b=0;
+#5 a=0;b=1;
+#5 a=1;b=0;    
+#5 a=1;b=1;
+#5
+$finish;
+end
+endmodule
