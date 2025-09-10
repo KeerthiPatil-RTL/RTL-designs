d81e0be7b7c9f42cb65116c1e58c62b9af0bb7e8
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-10 13:00:03 -0400

    Update Behavioral.v

--- HS/Behavioral.v
+++ HS/Behavioral.v
@@ -1,1 +1,11 @@
-
+module HS_BEH(input a,b,output reg B,D);
+always@(*) begin
+case ({a,b})
+ 2'b00 : begin D=0;B=0; end 
+ 2'b01 : begin D=1;B=1; end
+ 2'b10 : begin D=1;B=0; end
+ 2'b11 : begin D=0;B=0; end
+ default: begin D=0; B=0; end
+endcase    
+end
+endmodule
