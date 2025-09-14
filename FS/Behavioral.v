205d92552bfc783d5bcadbef7295fca3e21de6e5
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-14 19:08:29 -0400

    Update Behavioral.v

--- FS/Behavioral.v
+++ FS/Behavioral.v
@@ -1,11 +1,12 @@
+
 module subbeh(input a,b,c,output reg d,B);
 always @(*)  begin
 case({a,b,c}) 
 3'b000 : begin d=0 ; B=0 ;end 
 3'b001 : begin d=1 ; B=1 ;end
 3'b010 : begin d=1 ; B=1 ;end
-3'b011 : begin d=1 ; B=0 ;end
-3'b100 : begin d=0 ; B=1 ;end
+3'b011 : begin d=0 ; B=1 ;end
+3'b100 : begin d=1 ; B=0 ;end
 3'b101 : begin d=0 ; B=0 ;end
 3'b110 : begin d=0 ; B=0 ;end    
 3'b111 : begin d=1 ; B=1 ;end
