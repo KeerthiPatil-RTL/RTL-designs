e3796c64d8200e57f6bb1f64592dd463b50203c7
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-16 07:50:43 -0400

    Update demux1to2_structural.v

--- DEMUX/demux1to2_structural.v
+++ DEMUX/demux1to2_structural.v
@@ -1,1 +1,11 @@
+module demux1to2_struct (
+    input I,    
+    input S,    
+    output Y0, Y1
+);
+    wire nS;
+    not (nS, S);        
+    and (Y0, I, nS);    
+    and (Y1, I, S);     
+endmodule
 
