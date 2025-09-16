b70c9c82368fa0759b564ebaa9de6bc975fb88b5
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-16 07:51:40 -0400

    Update demux1to2_Behavioral.v

--- DEMUX/demux1to2_Behavioral.v
+++ DEMUX/demux1to2_Behavioral.v
@@ -1,1 +1,14 @@
+module demux1to2_beh (
+    input I,
+    input S,
+    output reg [1:0] Y
+);
+    always @(*) begin
+        case (S)
+            1'b0: Y = {1'b0, I};  
+            1'b1: Y = {I, 1'b0}; 
+            default: Y = 2'b00;
+        endcase
+    end
+endmodule
 
