7560733233f44baef7f43bac31450a5c9176d73b
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-15 22:33:14 -0400

    Create muxNto1_tb.v

---
+++ MUX/muxNto1_tb.v
@@ -0,0 +1,21 @@
+module mux_nto1_tb;
+    reg  [7:0] I;
+    reg  [2:0] S;     
+    wire Y;
+mux_nto1 #(.N(8)) DUT (.I(I), .S(S), .Y(Y));
+initial begin
+        $dumpfile("mux_nto1.vcd");
+        $dumpvars(0, mux_nto1_tb);
+        $monitor("time=%0t | S=%b | I=%b | Y=%b", $time, S, I, Y);
+        I = 8'b1010_1101;   
+        S = 3'b000; #5;
+        S = 3'b001; #5;
+        S = 3'b010; #5;
+        S = 3'b011; #5;
+        S = 3'b100; #5;
+        S = 3'b101; #5;
+        S = 3'b110; #5;
+        S = 3'b111; #5;
+        $finish;
+    end
+endmodule
