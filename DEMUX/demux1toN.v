d13e47e2cbc1f38da5e3c0d9510edd57a5ee1a15
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-16 07:55:04 -0400

    Update demux1toN.v

--- DEMUX/demux1toN.v
+++ DEMUX/demux1toN.v
@@ -1,1 +1,11 @@
+module demux_1toN #(parameter N = 8)(
+    input I,                       
+    input [$clog2(N)-1:0] S,         
+    output reg [N-1:0] Y            
+);
+    always @(*) begin
+        Y = {N{1'b0}};   
+        Y[S] = I;          
+    end  
+endmodule
 
