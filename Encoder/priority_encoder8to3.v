aa7bb8c1ea12981dcc928cf1f0ed061d697068df
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-16 13:16:57 -0400

    Update priority_encoder8to3.v

--- Encoder/priority_encoder8to3.v
+++ Encoder/priority_encoder8to3.v
@@ -1,1 +1,23 @@
-
+module priority_encoder8to3_beh (
+    input  [7:0] I,
+    output reg [2:0] Y,
+    output reg valid    
+);
+    always @(*) begin
+        valid = 1'b1;  
+        casex (I)
+            8'b1xxxxxxx: Y = 3'b111; 
+            8'b01xxxxxx: Y = 3'b110; 
+            8'b001xxxxx: Y = 3'b101; 
+            8'b0001xxxx: Y = 3'b100;
+            8'b00001xxx: Y = 3'b011;
+            8'b000001xx: Y = 3'b010; 
+            8'b0000001x: Y = 3'b001; 
+            8'b00000001: Y = 3'b000; 
+            default: begin
+                Y = 3'b000; 
+                valid = 1'b0;
+            end
+        endcase
+    end
+endmodule
