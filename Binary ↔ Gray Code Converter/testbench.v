1d4983553133fb6f34f98e5a073e6584abc22f62
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-17 15:01:06 -0400

    Update testbench.v

--- Binary ↔ Gray Code Converter/testbench.v
+++ Binary ↔ Gray Code Converter/testbench.v
@@ -1,1 +1,12 @@
-
+module bintogray_tb;
+    reg [3:0] B;
+    wire [3:0] G;
+bintogray DUT(.B(B), .G(G));
+initial begin
+$monitor("time= %0t, B= %b, G=%b",$time,B,G);
+B=4'b0010; #5;
+B=4'b1101;#5;
+B=4'b1010;#5;
+$finish;
+end
+endmodule
