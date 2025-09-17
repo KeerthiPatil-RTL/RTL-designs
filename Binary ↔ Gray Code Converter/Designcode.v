fce063f1acb4b463e051f99dc80f2e9d48fa16e3
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-17 15:00:49 -0400

    Update Designcode.v

--- Binary ↔ Gray Code Converter/Designcode.v
+++ Binary ↔ Gray Code Converter/Designcode.v
@@ -1,1 +1,12 @@
-
+module bintogray(
+    input  [3:0] B,
+    output [3:0] G
+);
+  assign G[3] = B[3]; 
+genvar i;
+generate
+    for (i = 1; i < 4; i = i+1) begin : gray_loop
+        assign G[i-1] = B[i] ^ B[i-1];
+    end
+endgenerate
+endmodule
