a884c35a8eff9ec33024f7d10415466855a874bf
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-17 23:48:37 -0400

    Update bcd_adder.v

--- BCD_Adder/bcd_adder.v
+++ BCD_Adder/bcd_adder.v
@@ -1,1 +1,15 @@
+module bcd_adder (
+    input  [3:0] A,    
+    input  [3:0] B,    
+    output reg [3:0] SUM  
+);
+    reg [4:0] temp;
+    always @(*) begin
+        temp = A + B;
+        if (temp > 9)
+            SUM = temp + 6;  
+        else
+            SUM = temp[3:0];  
+    end
+endmodule
 
