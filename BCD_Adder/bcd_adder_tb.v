6a7db5d4f3e28e90965ab1b17ba6bd383fb1f452
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-17 23:49:04 -0400

    Update bcd_adder_tb.v

--- BCD_Adder/bcd_adder_tb.v
+++ BCD_Adder/bcd_adder_tb.v
@@ -1,1 +1,14 @@
-
+module bcd_adder_tb;
+    reg  [3:0] A, B;
+    wire [3:0] SUM;
+    bcd_adder DUT (.A(A), .B(B), .SUM(SUM));
+    initial begin
+        $monitor("time=%0t | A=%d B=%d | SUM=%d",
+                  $time, A, B, SUM);
+        A=4'd3; B=4'd4; #5;  
+        A=4'd7; B=4'd5; #5;   
+        A=4'd9; B=4'd6; #5;   
+        A=4'd8; B=4'd2; #5;  
+        $stop;
+    end
+endmodule
