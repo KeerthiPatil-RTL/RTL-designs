d09dec1dcacf311dcd53e6a6793283dda475777e
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 09:49:43 -0500

    Update johnson_counter.v

--- Johnson_Counter/johnson_counter.v
+++ Johnson_Counter/johnson_counter.v
@@ -1,8 +1,8 @@
-module ringcounter(input clk,rst,output reg [3:0]count);
+module johnsoncounter(input clk,rst,output reg [3:0]count);
 always@(posedge clk)begin
 if(rst)
-count<=4'b0001;
+count<=4'b0000;
 else 
-count<={count[2:0],count[3]};
+count<={count[2:0],~count[3]};
 end
-endmodule 
+endmodule   
