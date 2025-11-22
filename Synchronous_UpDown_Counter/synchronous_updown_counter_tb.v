f6af177750ccd0230f1b88f1d7b55709d9a3e943
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 08:37:35 -0500

    Add testbench for synchronous up/down counter

--- Synchronous_UpDown_Counter/synchronous_updown_counter_tb.v
+++ Synchronous_UpDown_Counter/synchronous_updown_counter_tb.v
@@ -1,1 +1,14 @@
-
+module tb_counter;
+reg clk,rst,updown;
+wire [3:0]count;
+counter uut(.clk(clk), .rst(rst),.updown(updown),.count(count));
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+rst=1;
+$display("time=%0t, clk=%b,rst=%b,updown=%d",$time,clk,rst,count,updown);
+#10; rst=0;updown=1'b0;
+#60 ; rst=1;updown=1'b1;
+#10; rst=0;updown=1'b1;
+#100; $finish; end
+endmodule
