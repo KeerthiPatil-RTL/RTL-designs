114b10b6198460716603b5b17e4f3a6b7516123c
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 14:41:27 -0500

    Add testbench for modNcounter module

--- ModN_Counter/modn_counter_tb.v
+++ ModN_Counter/modn_counter_tb.v
@@ -1,1 +1,15 @@
 
+module tb_modNcounter;
+parameter N=10;
+reg clk,rst;
+wire [$clog2(N)-1:0]count;
+modNcounter #(.N(10)) uut(.clk(clk), .rst(rst),.count(count));
+initial clk=0;  
+always #2 clk=~clk;  
+initial begin   
+rst=1;
+$display("time=%0t, clk=%b,rst=%b,count =%b",$time,clk,rst,count);
+#10; rst=0;   
+#100 $finish; 
+end              
+endmodule 
