1f24514936e8f864b70c5787841972596ff7a69d
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 09:49:57 -0500

    Update johnson_counter_tb.v

--- Johnson_Counter/johnson_counter_tb.v
+++ Johnson_Counter/johnson_counter_tb.v
@@ -1,14 +1,13 @@
-module tb_ring_counter;
+module tb_johnson_counter;
 reg clk,rst;
 wire [3:0]count;
-ringcounter uut(.clk(clk), .rst(rst),.count(count));
-initial clk=0;
+johnsoncounter uut(.clk(clk), .rst(rst),.count(count));
+initial clk=0;  
 always #2 clk=~clk;
 initial begin   
 rst=1;
-$display("time=%0t, clk=%b,rst=%b",$time,clk,rst,count);
-#10; rst=0;
-#100; $finish; 
-end            
+$display("time=%0t, clk=%b,rst=%b,count =%b",$time,clk,rst,count);
+#10; rst=0;   
+#100 $finish; 
+end              
 endmodule
-
