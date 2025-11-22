7431995875267e5c648aaa86b7e2206252a199fc
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 09:06:55 -0500

    Update asynchronous_updown_counter_tb.v

--- Asynchronous_UpDown_Counter/asynchronous_updown_counter_tb.v
+++ Asynchronous_UpDown_Counter/asynchronous_updown_counter_tb.v
@@ -1,1 +1,17 @@
+module tb_counter;
+reg clk,rst,updown;
+wire [3:0]count;
+counter uut(.clk(clk), .rst(rst),.updown(updown),.count(count));
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+rst=1;updown=1;
+$display("time=%0t, clk=%b,rst=%b,updown=%d",$time,clk,rst,count,updown);
+#100; rst=0;updown=1'b0;
+#15 ; rst=1;updown=1'b1;
+#10; rst=0;updown=1'b1;
+#100; $finish; end
+endmodule
+
+
 
