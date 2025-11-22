0da9708ca70844bba0ce86b503a7f47441f4cc91
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-21 19:23:30 -0500

    Add testbench for SR flip-flop module

--- SR_FF/SRFF_tb.v
+++ SR_FF/SRFF_tb.v
@@ -1,1 +1,15 @@
+module srff_tb;
+ reg s,r,clk;
+ wire q ;
+ srff uut(.s(s),.r(r),.clk(clk),.q(q));
+initial clk =0 ;
+ always #5 clk= ~clk;
+  initial begin
+$display("time = %0t | s = %b | r = %b |clk = %b | q = %q", $time,s,r,clk,q);
+#10 s=0;r=0;
+#10 s=0;r=1;
+#10 s=1;r=0;
+#10  s=0;r=1;
+ end
+endmodule
 
