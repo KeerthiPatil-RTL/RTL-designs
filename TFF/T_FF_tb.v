4f270beaef84cdfadfa27a70b14910edf607c5ee
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-21 20:46:06 -0500

    Add testbench for T flip-flop module

--- TFF/T_FF_tb.v
+++ TFF/T_FF_tb.v
@@ -1,1 +1,14 @@
-
+module tff_tb;
+reg t,clk;
+wire q;
+tff uut(.t(t), .clk(clk),.q(q));
+initial clk=0;
+always #5 clk=~clk;
+initial begin
+$display("time = %0t | t=%b | clk= %b | q=%b", $time,t,clk,q);
+#10;t=0;
+#10;t=1;
+#10; t=0;
+#50;$finish;
+end
+endmodule
