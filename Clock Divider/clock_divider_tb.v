dadf57b3c257e8c1097de53df95e87e52f16595c
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 15:56:43 -0500

    Add testbench for clock divider module

--- Clock Divider/clock_divider_tb.v
+++ Clock Divider/clock_divider_tb.v
@@ -1,1 +1,19 @@
+module tb_clock_divider;
 
+reg clk, rst;
+wire clk_out;
+
+clock_divider #(.DIVIDE_BY(10)) uut (
+    .clk(clk), .rst(rst), .clk_out(clk_out)
+);
+
+initial clk = 0;
+always #1 clk = ~clk;   
+
+initial begin
+    rst = 1;
+    #10 rst = 0;
+    #200 $finish;
+end
+
+endmodule
