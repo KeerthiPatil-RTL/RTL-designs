43fac8cb5161070cf707674eb0a9af61c52bc137
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-21 20:46:50 -0500

    Update JKFF_tb.v

--- JKFF/JKFF_tb.v
+++ JKFF/JKFF_tb.v
@@ -1,1 +1,16 @@
-
+module jkff_tb;
+reg j,k,clk;
+wire q;
+jk_ff uut(.j(j), .k(k),.clk(clk),.q(q));
+initial clk=0;
+always #5 clk =~clk;
+initial begin
+$display("time = %0t | j = %b | k = %b| clk = %b | q = %b ", $time,j,k,clk,q);
+j=0;k=0;
+#10; j=0;k=0;
+#10; j=0;k=1;
+#10; j=1;k=0;  
+#10; j=1;k=1;
+#10;$finish;
+end
+endmodule
