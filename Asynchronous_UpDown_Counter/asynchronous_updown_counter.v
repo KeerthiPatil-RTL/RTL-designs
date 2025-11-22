012160ef3552cae4a8f273e10cd128ae8e2158f9
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 09:07:20 -0500

    Update asynchronous_updown_counter.v

--- Asynchronous_UpDown_Counter/asynchronous_updown_counter.v
+++ Asynchronous_UpDown_Counter/asynchronous_updown_counter.v
@@ -1,1 +1,10 @@
-
+module counter(input clk,rst,updown,output reg [3:0]count);
+  always@(posedge clk or posedge rst)begin
+if(rst)
+count<=4'b0000;
+else if(updown) 
+count<=count+1;
+else 
+count<=count-1;
+end
+endmodule  
