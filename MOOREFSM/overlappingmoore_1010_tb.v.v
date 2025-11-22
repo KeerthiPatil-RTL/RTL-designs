bfe9ff85e8a8000b0b6481d5ca5ccc7fd5bd807c
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 18:39:21 -0500

    Update overlappingmoore_1010_tb.v.v

--- MOOREFSM/overlappingmoore_1010_tb.v.v
+++ MOOREFSM/overlappingmoore_1010_tb.v.v
@@ -1,1 +1,56 @@
+module moore_1010(input clk,rst,in, output reg out);
+parameter s0=0,s1=1,s2=2,s3=3,s4=4;
+reg [2:0] ps,ns;
+
+always@(posedge clk)begin
+if(rst) begin
+out<=0;
+ps <= s0;
+end
+else begin
+ps<=ns;
+end
+end
+
+always @(*) begin
+case(ps)
+s0: begin
+out=0;
+ns = in ? s1:s0;
+end
+
+s1: begin
+out=0;
+ns = in ? s1:s2;
+end
+
+s2: begin
+out=0;
+ns = in ? s3:s0;
+end
+
+s3: begin
+out=0;
+ns = in ? s1:s4;  
+end
+
+s4: begin
+out=1;
+ns = in ? s3:s0;
+end
+
+default: begin
+out=0;
+ns = s0;
+end
+
+endcase
+end
+endmodule
+
+
+
+
+
+
 
