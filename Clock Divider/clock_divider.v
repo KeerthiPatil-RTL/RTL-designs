a4afa15e15e50d49f99a74f090fab0d67e708622
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 15:56:32 -0500

    Update clock_divider.v

--- Clock Divider/clock_divider.v
+++ Clock Divider/clock_divider.v
@@ -1,1 +1,26 @@
+module clock_divider #(
+    parameter DIVIDE_BY = 10
+)(
+    input clk,
+    input rst,
+    output reg clk_out
+);
+
+reg [$clog2(DIVIDE_BY)-1:0] count;
+
+always @(posedge clk) begin
+    if (rst) begin
+        count   <= 0;
+        clk_out <= 0;
+    end else begin
+        if (count == DIVIDE_BY-1) begin
+            count   <= 0;
+            clk_out <= ~clk_out;     
+        end else begin
+            count <= count + 1;
+        end
+    end
+end
+
+endmodule
 
