ace9bf2cb90c8b3c5abc9e7a001c748035811be2
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-21 19:45:42 -0500

    Refactor SRFF testbench for better output monitoring
    Updated testbench to use $monitor for output and adjusted stimulus timing.

--- SR_FF/SRFF_tb.v
+++ SR_FF/SRFF_tb.v
@@ -2,14 +2,17 @@ module srff_tb;
  reg s,r,clk;
  wire q ;
  srff uut(.s(s),.r(r),.clk(clk),.q(q));
+ 
 initial clk =0 ;
  always #5 clk= ~clk;
   initial begin
-$display("time = %0t | s = %b | r = %b |clk = %b | q = %q", $time,s,r,clk,q);
-#10 s=0;r=0;
-#10 s=0;r=1;
-#10 s=1;r=0;
-#10  s=0;r=1;
+  $monitor("time = %0t | s = %b | r = %b |clk = %b | q = %b", $time,s,r,clk,q);
+s=0;r=1;
+#20 s=0;r=0;
+#20 s=0;r=1;
+#20 s=1;r=0;
+#20  s=1;r=1;  
+#50; $finish;
  end
 endmodule
 
