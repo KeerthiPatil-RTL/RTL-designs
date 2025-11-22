277482ddd2e307bcb1b1d1b35c3659651ed56d93
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-21 22:52:01 -0500

    Update synchronous_up_counter_tb.v

--- Synchronous_Up_Counter/synchronous_up_counter_tb.v
+++ Synchronous_Up_Counter/synchronous_up_counter_tb.v
@@ -1,1 +1,15 @@
-
+module tb_counter;
+reg clk,rst;
+wire [3:0]count;
+counter uut(.clk(clk), .rst(rst), .count(count));
+initial clk=0;
+always #5 clk=~clk;
+initial begin
+rst=1;
+$display("time=%0t, clk=%b,rst=%b,count=%d",$time,clk,rst,count);
+#20; rst=0;
+#160 ; rst=1;
+#190;
+$finish;
+end
+endmodule
