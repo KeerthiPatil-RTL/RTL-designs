e37d535ad4c5d58a8cbedd924c322019c0187a00
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 09:08:07 -0500

    Update synchronous_updown_counter_tb.v

--- Synchronous_UpDown_Counter/synchronous_updown_counter_tb.v
+++ Synchronous_UpDown_Counter/synchronous_updown_counter_tb.v
@@ -5,10 +5,10 @@ counter uut(.clk(clk), .rst(rst),.updown(updown),.count(count));
 initial clk=0;
 always #2 clk=~clk;
 initial begin
-rst=1;
+rst=1;updown=1;
 $display("time=%0t, clk=%b,rst=%b,updown=%d",$time,clk,rst,count,updown);
-#10; rst=0;updown=1'b0;
-#60 ; rst=1;updown=1'b1;
+#100; rst=0;updown=1'b0;
+#15 ; rst=1;updown=1'b1;
 #10; rst=0;updown=1'b1;
 #100; $finish; end
 endmodule
