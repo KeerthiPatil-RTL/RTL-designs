e7b236e8362853603583535eb7b456320cd9494c
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-22 14:41:13 -0500

    Update modn_counter.v

--- ModN_Counter/modn_counter.v
+++ ModN_Counter/modn_counter.v
@@ -1,1 +1,11 @@
 
+module modNcounter #(parameter N=10)(input clk,rst,output reg [$clog2(N)-1:0]count);
+always@(posedge clk)begin
+if(rst)
+count<=4'b0000;
+else if (count == N-1) 
+count<= 4'b000;
+else 
+count<=count+1;
+end
+endmodule 
