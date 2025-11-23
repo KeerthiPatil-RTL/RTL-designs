a94d37b889d8ec4569bc4d5175c3355abf80748d
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-23 11:36:34 -0500

    Delete VERIRISC_CPU_VERILOG directory

--- VERIRISC_CPU_VERILOG/mux.v
+++
@@ -1,4 +0,0 @@
-module mux #(parameter width = 5)
-  (input [width-1:0] in0,in1,input sel , output wire [width-1:0] mux_out);  
-  assign mux_out = sel ? in1 : in0; //sel=0 in=0 or sel=1 in1
-endmodule
